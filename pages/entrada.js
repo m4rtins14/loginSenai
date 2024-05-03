fd5745da3df2cc53fe3f746b1dcd63ebe67fb8e2
m4rtins14 <133423369+m4rtins14@users.noreply.github.com>
2024-05-03 10:59:33 -0300

    Add files via upload

---
+++ pages/entrada.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
+    TouchableOpacity,
+    KeyboardAvoidingView,
+    Keyboard,
+    TouchableWithoutFeedback
+} from 'react-native';
+import * as Animatable from 'react-native-animatable';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+export default function Acesso() {
+    const navigation = useNavigation();
+
+    return (
+        <KeyboardAvoidingView style={{ flex: 1 }}>
+            <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+                <View style={styles.container}>
+                    <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
+                        <Ionicons name="arrow-back" size={24} color="white" />
+                    </TouchableOpacity>
+                    <Animatable.View animation="fadeInLeft" delay={500} style={styles.containerHeader}>
+                        <Text style={styles.message}>Bem-vindo(a)</Text>
+                    </Animatable.View>
+                    <Animatable.View animation="fadeInUp" style={styles.containerForm}>
+                        <Text style={styles.title}>E-mail</Text>
+                        <TextInput placeholder='Digite um email...' style={styles.input} />
+                        <TextInput placeholder='Sua senha' style={styles.input} />
+                        <TouchableOpacity style={styles.button}>
+                            <Text style={styles.buttonText}>Acessar</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.buttonRegister}>
+                            <Text style={styles.registerText}>Não possui uma conta? Cadastre-se</Text>
+                        </TouchableOpacity>
+                    </Animatable.View>
+                </View>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#880000'
+    },
+    containerHeader: {
+        marginTop: '14%',
+        marginBottom: '8%',
+        paddingStart: '5%',
+    },
+    message: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: "#FFF"
+    },
+    containerForm: {
+        backgroundColor: "#FFF",
+        flex: 1,
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        paddingStart: "5%",
+        paddingEnd: "5%"
+    },
+    title: {
+        fontSize: 20,
+        marginTop: 28,
+    },
+    input: {
+        borderBottomWidth: 1,
+        height: 40,
+        marginBottom: 12,
+        fontSize: 16
+    },
+    button: {
+        backgroundColor: '#880000',
+        width: '100%',
+        borderRadius: 4,
+        paddingVertical: 8,
+        marginTop: 14,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: "#FFF",
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    buttonRegister: {
+        marginTop: 14,
+        alignSelf: 'center'
+    },
+    registerText: {
+        color: '#a1a1a1'
+    },
+    backButton: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        zIndex: 9999,
+        padding: 10
+    }
+});
